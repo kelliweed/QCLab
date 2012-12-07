57a8e9d1a1269aca11c207af69eecf449d437f3a
Campbell <campbell@bikalabs.com>
2012-12-07 15:31:02 +0200

    Batch workflow

--- bika/lims/content/batch.py
+++ bika/lims/content/batch.py
@@ -10,11 +10,13 @@ from bika.lims.browser.fields import DurationField
 from bika.lims.config import PROJECTNAME
 from bika.lims.content.bikaschema import BikaSchema
 from bika.lims.interfaces import IBatch
-from datetime import timedelta
 from bika.lims.utils import isActive
+from bika.lims.workflow import doActionFor
+from bika.lims.workflow import skip
+from datetime import timedelta
 from zope.interface import implements
-import plone
 import json
+import plone
 
 schema = BikaSchema.copy() + Schema((
     StringField('BatchID',
@@ -38,7 +40,7 @@ schema = BikaSchema.copy() + Schema((
             label=_("Batch labels"),
             format="checkbox",
         )
-    ),    
+    ),
     TextField('Remarks',
         searchable=True,
         default_content_type='text/x-web-intelligent',
@@ -124,9 +126,86 @@ class Batch(BaseContent):
 
     def getAnalysisRequests(self):
         bc = getToolByName(self, 'bika_catalog')
-        uid = self.context.UID()
-        return [b.getObject() for b in bc(portal_type='AnalysisRequest',
-                                          getBatchUID=uid)]
+        uid = self.UID()
+        return [b.getObject() for b in bc(portal_type='AnalysisRequest', getBatchUID=uid)]
+
+    def workflow_guard_receive(self):
+        """Permitted when all Samples are > sample_received
+        """
+        wf = getToolByName(self, 'portal_workflow')
+        states = ['sample_registered', 'to_be_sampled', 'sampled', 'to_be_preserved', 'sample_due']
+        import pdb;pdb.set_trace()
+        for o in self.getAnalysisRequests():
+            if wf.getInfoFor(o, 'review_state') in states:
+                return False
+        return True
+
+    # def workflow_before_receive(self, state_info):
+    #     pass
+
+    def workflow_after_receive(self, state_info):
+        skip(self, 'receive')
+
+    def workflow_guard_open(self):
+        """Permitted when at least one sample is < sample_received
+        """
+        wf = getToolByName(self, 'portal_workflow')
+        states = ['sample_registered', 'to_be_sampled', 'sampled', 'to_be_preserved', 'sample_due']
+        for o in self.getAnalysisRequests():
+            if wf.getInfoFor(o, 'review_state') in states:
+                return True
+        return False
+
+    # def workflow_before_open(self, state_info):
+    #     pass
+
+    def workflow_after_open(self, state_info):
+        skip(self, 'open')
+        # reset everything and return to open state
+        self.setDateReceived(None)
+        self.reindexObject(idxs = ["getDateReceived", ])
+
+    def workflow_guard_submit(self):
+        """Permitted when all samples >= to_be_verified
+        """
+        wf = getToolByName(self, 'portal_workflow')
+        states = ['sample_registered', 'to_be_sampled', 'sampled', 'to_be_preserved', 'sample_due', 'sample_received']
+        for o in self.getAnalysisRequests():
+            if wf.getInfoFor(o, 'review_state') in states:
+                return False
+        return True
+
+    # def workflow_before_submit(self, state_info):
+    #     pass
+
+    # def workflow_after_submit(self, state_info):
+    #     skip(self, 'open')
+
+    def workflow_guard_verify(self):
+        """Permitted when all samples >= verified
+        """
+        wf = getToolByName(self, 'portal_workflow')
+        states = ['sample_registered', 'to_be_sampled', 'sampled', 'to_be_preserved', 'sample_due', 'sample_received',
+                  'to_be_verified']
+        for o in self.getAnalysisRequests():
+            if wf.getInfoFor(o, 'review_state') in states:
+                return False
+        return True
+
+    # def workflow_before_verify(self, state_info):
+    #     pass
+
+    # def workflow_after_verify(self, state_info):
+    #     skip(self, 'open')
+
+    # def workflow_guard_close(self):
+    #     return True
+
+    # def workflow_before_close(self, state_info):
+    #     pass
+
+    # def workflow_after_close(self, state_info):
+    #     skip(self, 'open')
 
 
 registerType(Batch, PROJECTNAME)
