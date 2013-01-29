b3878d1d0393284e9f018feccd95f5a97f91f8e7
Campbell <campbell@bikalabs.com>
2013-01-29 17:12:17 +0200

    Fix bad merge from master

--- bika/lims/browser/client.py
+++ bika/lims/browser/client.py
@@ -11,7 +11,6 @@ from bika.lims.browser.analysisrequest import AnalysisRequestWorkflowAction, \
 from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.content.patients import PatientsView
 from bika.lims.browser.publish import doPublish
-from bika.lims.browser.publish import Publish
 from bika.lims.browser.sample import SamplesView
 from bika.lims.interfaces import IContacts
 from bika.lims.permissions import *
