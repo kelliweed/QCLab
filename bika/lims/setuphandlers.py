e3ed2456be28fdd18aafd122bf5a659a71bd3ae5
Ammy2 <aman2arora2@gmail.com>
2015-06-19 18:05:16 +0900

    Permission assigned to Lab Clerk as well

--- bika/lims/setuphandlers.py
+++ bika/lims/setuphandlers.py
@@ -432,11 +432,11 @@ class BikaGenerator:
             # /orders folder permissions
             mp = portal.orders.manage_permission
             mp(CancelAndReinstate, ['Manager', 'LabManager', 'LabClerk'], 0)
-            mp(AddInventoryOrder, ['Manager', 'LabManager', 'Owner'], 1)
+            mp(AddInventoryOrder, ['Manager', 'LabManager', 'Owner', 'LabClerk'], 1)
             mp(permissions.ListFolderContents, ['Member'], 1)
-            mp(permissions.AddPortalContent, ['Manager', 'LabManager', 'Owner'], 0)
-            mp(permissions.DeleteObjects, ['Manager', 'LabManager', 'Owner'], 0)
-            mp(permissions.View, ['Manager', 'LabManager'], 0)
+            mp(permissions.AddPortalContent, ['Manager', 'LabManager', 'Owner', 'LabClerk'], 0)
+            mp(permissions.DeleteObjects, ['Manager', 'LabManager', 'Owner', 'LabClerk'], 0)
+            mp(permissions.View, ['Manager', 'LabManager', 'LabClerk'], 0)
             portal.orders.reindexObject()
         except:
             pass
