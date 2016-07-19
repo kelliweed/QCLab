27b87cccf9f1423cedb8f8df228349e70f6e0f94
Campbell <campbell@bikalabs.com>
2016-07-19 17:16:43 +0200

    AddStorageUnits viewlet, to quickly add a set of Storage Units at /storage

---
+++ bika/lims/browser/storage/addstorageunits_viewlet.py
@@ -0,0 +1,116 @@
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+from plone.app.layout.viewlets import ViewletBase
+
+
+class AddStorageUnitsViewlet(ViewletBase):
+    index = ViewPageTemplateFile("templates/addstorageunits_viewlet.pt")
+
+    def render(self):
+        return self.index()
+
+
+class AddStorageUnitsSubmitHandler(BrowserView):
+    """
+    """
+
+    def __call__(self):
+
+        if "viewlet_submitted" in self.request.form:
+            # Title and ID Templates
+            titletemplate = self.request.form.get('titletemplate', None)
+            idtemplate = self.request.form.get('idtemplate', None)
+            if not (titletemplate and idtemplate):
+                self.form_error(u'ID and Title template are both required.')
+                return
+            if not ('{id}' in titletemplate and '{id}' in idtemplate):
+                self.form_error(u'ID and Title templates must contain {id} '
+                                u'for ID sequence substitution')
+                return
+
+            try:
+                seq_start = int(self.request.form.get('seq_start', None))
+                storageunit_count = int(
+                    self.request.form.get('storageunit_count', None))
+                storagelocation_count = int(
+                    self.request.form.get('storagelocation_count', None))
+            except:
+                self.form_error(u'Sequence start and all counts must '
+                                u'be integers')
+                self.request.response.redirect(self.context.absolute_url())
+                return
+
+            # verify ID sequence start
+            if seq_start < 1:
+                self.form_error(u'Sequence Start may not be < 1')
+                self.request.response.redirect(self.context.absolute_url())
+                return
+
+            # verify StorageUnit
+            if storageunit_count < 1:
+                self.form_error(u'Storage Unit count must not be < 1')
+                self.request.response.redirect(self.context.absolute_url())
+                return
+
+            # verify StorageLocation count
+            if storagelocation_count < 0:
+                self.form_error(u'Storagelocation count may not be < 0')
+                self.request.response.redirect(self.context.absolute_url())
+                return
+
+            # verify storage_type interface selection
+            storage_types = self.request.form.get('storage_type', [])
+            if any([storage_types, storagelocation_count]) \
+                    and not all([storage_types, storagelocation_count]):
+                self.form_error(u'To create Storage locations in the new '
+                                u'units, at least one Storage Type must be '
+                                u'selected.')
+                self.request.response.redirect(self.context.absolute_url())
+                return
+
+            # Check that none of the IDs conflict with existing items
+            ids = [x.id for x in self.context.objectValues('StorageUnit')]
+            for x in range(storageunit_count):
+                check = idtemplate.format(id=seq_start + x)
+                if check in ids:
+                    self.form_error(
+                        u'The ID %s exists, cannot be created.' % check)
+                    self.request.response.redirect(self.context.absolute_url())
+                    return
+
+            # Create the new storage unit items
+            created_units = []
+            for x in range(seq_start, storageunit_count):
+                id = idtemplate.format(id=x+1)
+                title = titletemplate.format(id=x+1)
+                ob = api.content.create(
+                    container=self.context, type="StorageUnit",
+                    id=id, title=title)
+                self.context.manage_renameObject(ob.id, id)
+                created_units.append(ob)
+
+            # If required, create the StorageLocations inside the new units.
+            created_locations = []
+            if storagelocation_count:
+                for unit in created_units:
+                    for x in range(storagelocation_count):
+                        id = "pos-%s"%x
+                        title = "Position %s"%x
+                        ob = api.content.create(
+                            container=unit, type="StorageLocation",
+                            id=id, title=title)
+                        created_locations.append(ob)
+                        import pdb;pdb.set_trace()
+                        # Assign any selected storage type interfaces
+
+            msg = u'%s Storage units and %s storage locations created.' % (
+                len(created_units), len(created_locations))
+            self.context.plone_utils.addPortalMessage(msg)
+            self.request.response.redirect(self.context.absolute_url())
+
+
+    def form_error(self, msg):
+        self.context.plone_utils.addPortalMessage(msg)
+        self.request.response.redirect(self.context.absolute_url())
+        return msg
